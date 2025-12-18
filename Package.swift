14004e0b9b893340bca4883fa8ce1e5901ab2edb
Harold Serrano <harold.serrano@untoldengine.com>
2025-12-18 08:32:28 -0700

    [Patch] Updated SPM to use exact tag.

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
     ],
     dependencies: [
         // Use a branch during active development:
-        .package(url: "https://github.com/untoldengine/UntoldEngine.git", branch: "develop"),
+        //.package(url: "https://github.com/untoldengine/UntoldEngine.git", branch: "develop"),
         // Or pin to a release:
-        // .package(url: "https://github.com/untoldengine/UntoldEngine.git", from: "0.3.0")
+         .package(url: "https://github.com/untoldengine/UntoldEngine.git", exact: "0.6.0")
     ],
     targets: [
         .executableTarget(
