0f995776971fe7a1bbb30a41ac934d0d6d78d499
Untold Engine <harold.serrano@untoldengine.com>
2026-04-25 08:00:02 -0700

    [Patch] Updated package dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Use a branch during active development:
         //.package(url: "https://github.com/untoldengine/UntoldEngine.git", branch: "develop"),
         // Or pin to a release:
-        .package(url: "https://github.com/untoldengine/UntoldEngine.git", exact: "0.12.6"),
+        .package(url: "https://github.com/untoldengine/UntoldEngine.git", exact: "0.12.7"),
     ],
     targets: [
         .executableTarget(
