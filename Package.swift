f8a275aa168a45d72ad10a53c123520ca3c06378
Untold Engine <harold.serrano@untoldengine.com>
2026-09-16 06:29:36 -0700

    [Release] Preparing release 0.19.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Use a branch during active development:
         // .package(url: "https://github.com/untoldengine/UntoldEngine.git", branch: "develop"),
         // Or pin to a release:
-        .package(url: "https://github.com/untoldengine/UntoldEngine.git", branch: "develop"),
+        .package(url: "https://github.com/untoldengine/UntoldEngine.git", exact: "0.19.1"),
     ],
     targets: [
         .executableTarget(
